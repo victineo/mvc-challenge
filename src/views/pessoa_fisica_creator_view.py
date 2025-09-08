06571e3a7871756c29d550812e201f645b560346
Victor <victorhenrique.alves.lopes@gmail.com>
2025-09-08 19:45:06 -0300

    refactor: correcting ViewInterface import path

--- src/views/pessoa_fisica_creator_view.py
+++ src/views/pessoa_fisica_creator_view.py
@@ -1,4 +1,4 @@
-from .interfaces import ViewInterface
+from .interfaces.view_interface import ViewInterface
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 from src.controllers.interfaces.pessoa_fisica_creator_controller import PessoaFisicaCreatorControllerInterface
