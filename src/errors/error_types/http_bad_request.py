b384e2dcb6292dd31547a54c070573fa64ec018c
Victor <victorhenrique.alves.lopes@gmail.com>
2025-09-07 22:22:07 -0300

    feat: implementing custom error handling for common errors

---
+++ src/errors/error_types/http_bad_request.py
@@ -0,0 +1,6 @@
+class HttpBadRequestError(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.status_code = 400
+        self.name = "BadRequest"
+        self.message = message
