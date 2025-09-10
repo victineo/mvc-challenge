d5bc17ee75f0688903c3a1782651b3d925e09038
Victor <victorhenrique.alves.lopes@gmail.com>
2025-09-10 19:16:07 -0300

    refactor: renaming funcions on physical person repository interface

--- src/models/sqlite/interfaces/pessoa_fisica_repository.py
+++ src/models/sqlite/interfaces/pessoa_fisica_repository.py
@@ -3,11 +3,11 @@ from src.models.sqlite.entities.pessoa_fisica import PessoaFisica
 
 class PessoaFisicaRepositoryInterface(ABC):
     @abstractmethod
-    def create_pessoa_fisica(self, renda_mensal: float, idade: int, nome_completo: str, celular: str, email: str, categoria: str, saldo: float) -> None:
+    def criar_pessoa_fisica(self, renda_mensal: float, idade: int, nome_completo: str, celular: str, email: str, categoria: str, saldo: float) -> None:
         pass
 
     @abstractmethod
-    def get_pessoa_fisica(self, person_id: int) -> PessoaFisica | None:
+    def obter_pessoa_fisica(self, person_id: int) -> PessoaFisica | None:
         pass
 
     @abstractmethod
