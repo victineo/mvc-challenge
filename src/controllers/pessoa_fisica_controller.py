8cb0d43c3663fedf08ab7a40a1bb222c5fd29cfd
Victor <victorhenrique.alves.lopes@gmail.com>
2025-09-16 00:48:53 -0300

    refactor: simplifying controller methods
    Getting rid of some private methods and using built-in 'to_dict' method from 'PessoaFisica' class instead

--- src/controllers/pessoa_fisica_controller.py
+++ src/controllers/pessoa_fisica_controller.py
@@ -19,70 +19,27 @@ class PessoaFisicaController(PessoaFisicaControllerInterface):
         saldo = person_info['saldo']
 
         self.__validate_complete_name(nome_completo)
-        self.__insert_pessoa_fisica_in_db(renda_mensal, idade, nome_completo, celular, email, categoria, saldo)
+        person_data = self.__pessoa_fisica_repository.create_pessoa_fisica(renda_mensal, idade, nome_completo, celular, email, categoria, saldo)
 
-        return self.__format_response(person_info)
+        return person_data.to_dict()
 
     def buscar_pessoa_fisica(self, person_id: int) -> Dict:
-        person = self.__find_pessoa_fisica_in_db(person_id)
+        person = self.__pessoa_fisica_repository.get_pessoa_fisica(person_id)
 
-        formatted_response = self.__format_response(person)
-        return formatted_response
+        if not person:
+            raise HttpNotFoundError("Pessoa Física não encontrada")
+
+        return person.to_dict()
 
     def sacar_dinheiro(self, person_id: int, quantidade: float) -> Dict:
-        person = self.__sacar_dinheiro_in_db(person_id, quantidade)
+        person = self.__pessoa_fisica_repository.withdraw_money(person_id, quantidade)
 
-        return self.__format_response(person)
+        return person.to_dict()
 
-    # -----
+    # ----------
 
     def __validate_complete_name(self, complete_name: str) -> None:
         valid_characters = re.compile(r'^[a-zA-Z\s]+$')
 
         if not valid_characters.search(complete_name):
             raise ValueError("Nome completo deve conter apenas letras e espaços")
-
-    def __insert_pessoa_fisica_in_db(self, renda_mensal: float, idade: int, nome_completo: str, celular: str, email: str, categoria: str, saldo: float) -> None:
-        self.__pessoa_fisica_repository.create_pessoa_fisica(renda_mensal, idade, nome_completo, celular, email, categoria, saldo)
-
-    def __find_pessoa_fisica_in_db(self, person_id: int) -> PessoaFisica | None:
-        person = self.__pessoa_fisica_repository.get_pessoa_fisica(person_id)
-
-        if not person:
-            raise HttpNotFoundError("Pessoa Física não encontrada")
-
-        return person
-
-    def __sacar_dinheiro_in_db(self, person_id: int, quantidade: float) -> PessoaFisica | None:
-        person = self.__pessoa_fisica_repository.withdraw_money(person_id, quantidade)
-
-        if not person:
-            raise HttpNotFoundError("Pessoa Física não encontrada")
-
-        return person
-
-    def __format_response(self, person: PessoaFisica | Dict | None) -> Dict:
-        if isinstance(person, Dict):
-            return {
-                "data": {
-                    "type": "pessoa_fisica",
-                    "count": 1,
-                    "attributes": person
-                }
-            }
-
-        return {
-            "data": {
-                "type": "pessoa_fisica",
-                "count": 1,
-                "attributes": {
-                    "renda_mensal": person.renda_mensal,
-                    "idade": person.idade,
-                    "nome_completo": person.nome_completo,
-                    "celular": person.celular,
-                    "email": person.email,
-                    "categoria": person.categoria,
-                    "saldo": person.saldo
-                }
-            }
-        }
