a3d99e92ac6857dfde4ae62a60810a9f9e8a0f02
Victor <victorhenrique.alves.lopes@gmail.com>
2025-09-17 23:57:40 -0300

    feat: adding built-in 'to_dict' method in legal entity

--- src/models/sqlite/entities/pessoa_juridica.py
+++ src/models/sqlite/entities/pessoa_juridica.py
@@ -1,9 +1,10 @@
+from typing import Dict
 from src.models.sqlite.settings.base import Base
 from sqlalchemy import Column, BIGINT, String, REAL
 
 class PessoaJuridica(Base):
     __tablename__ = "pessoa_juridica"
-    
+
     id = Column(BIGINT, primary_key=True)
     faturamento = Column(REAL, nullable=False)
     idade = Column(BIGINT, nullable=False)
@@ -15,3 +16,16 @@ class PessoaJuridica(Base):
 
     def __repr__(self):
         return f"PessoaJuridica[id={self.id}, nome={self.nome_fantasia}, email={self.email_corporativo}]"
+
+    def to_dict(self) -> Dict:
+        return {
+            "id": self.id,
+            "tipo": "pessoa_juridica",
+            "faturamento": self.faturamento,
+            "idade": self.idade,
+            "nome_fantasia": self.nome_fantasia,
+            "celular": self.celular,
+            "email_corporativo": self.email_corporativo,
+            "categoria": self.categoria,
+            "saldo": self.saldo
+        }
